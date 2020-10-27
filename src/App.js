3de871ad0a95916e67f1cc2913aebcd96a0bb877
K.Niroshani <niroshini.07.skk@gmail.com>
2020-10-27 07:36:44 +0530

    Import Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return ( 
     <div className="app">
       <h1>Hi</h1>
-
+      <Header />
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* React router */}
+      </div>
+      
     </div>
   );
 }
