d32106d97e6186f2141d2cb68e63b4ea72b00a69
K.Niroshani <niroshini.07.skk@gmail.com>
2020-10-27 07:39:23 +0530

    Import Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return ( 
@@ -8,7 +9,7 @@ function App() {
       <h1>Hi</h1>
       <Header />
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* React router */}
       </div>
       
