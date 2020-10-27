bed52b80dab8da2fce7f6cdbf6c6239c38967fa9
K.Niroshani <niroshini.07.skk@gmail.com>
2020-10-27 07:42:45 +0530

    ADD: firebase.js, connect to realtime database

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+// config goes here
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = firebase.auth.GoogleAuthProvider();
+
+export { auth, provider};
+export default db;
