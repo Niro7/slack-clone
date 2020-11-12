f533d59d220782097d3064ce832ea8202d5e2369
K.Niroshani <niroshini.07.skk@gmail.com>
2020-11-12 05:44:06 +0530

    ADD: Login functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Chat from './Chat';
+import Chat from "./Chat";
+import Login from "./Login";
+
 function App() {
-  return ( 
+  const [user, setUser] = useState(null);
+  return (
     <div className="app">
       <Router>
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-        </Switch>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
         {/* React router -> Chat Screen */}
-
-      </div>
       </Router>
-      
-      
     </div>
   );
 }
