b28b1526cff5a11b373fdceb3b082491eae28957
K.Niroshani <niroshini.07.skk@gmail.com>
2020-12-19 14:24:30 +0530

    UPDATE: fix bug

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-// config goes here
+  // config goes here
 };
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const provider = firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider};
 export default db;
